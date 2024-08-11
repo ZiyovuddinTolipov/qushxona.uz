bbbd5f5d70b7c521e78f45ba2be15e60965a9896
ziyovuddintolipov <ziyovuddin.developer@gmail.com>
2024-08-12 01:51:55 +0500

    head fix

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -2,7 +2,7 @@ import img from '../assets/bg.jpg'
 const Head = () => {
   return (
     <section className="h-auto md:h-[90svh]  text-white head-section relative" id="about">
-      <action className="mx-auto max-w-[1200px] h-auto md:h-[100svh] flex-col-reverse md:flex-row relative">
+      <action className="mx-auto max-w-[1200px] h-auto md:h-[100svh] flex flex-col-reverse md:flex-row relative">
         <div className="w-full md:w-1/2 flex flex-col justify-around px-5">
           <h1 className=" text-3xl text-white ">#01 Biz haqimizda</h1>
           <div className='text-white'>
