8126b58230ecf6e1c96a4094eabe07cdebb2156f
ziyovuddintolipov <ziyovuddin.developer@gmail.com>
2024-08-12 03:17:06 +0500

    overflowhidden;

--- src/components/Delivery.jsx
+++ src/components/Delivery.jsx
@@ -8,15 +8,15 @@ const Delivery = (props) => {
                 <h2 className=" text-2xl md:text-3xl py-5" data-aos="fade-right">{props.page} Buyurtma berish.</h2>
                 <form className="flex flex-col md:flex-row  gap-3 md:items-end w-full items-start">
                     <div className="flex flex-col max-w-sm text-black gap-y-1" data-aos="fade-right" data-aos-duration="900">
-                        <label htmlFor="name" className="text-white">Ismingiz</label>
+                        <label htmlFor="name" className="text-white max-w-sm">Ismingiz</label>
                         <input type="text" className="focus:outline-none px-3 py-2" />
                     </div>
                     <div className="flex flex-col max-w-sm text-black gap-y-1" data-aos="fade-up-right" data-aos-duration="900">
-                        <label htmlFor="name" className="text-white">Telefon raqamingiz</label>
+                        <label htmlFor="name" className="text-white max-w-sm">Telefon raqamingiz</label>
                         <input type="text" className="focus:outline-none px-3 py-2" />
                     </div>
                     <div className="flex flex-col max-w-sm text-black gap-y-1" data-aos="fade-up-left" data-aos-duration="900">
-                        <label htmlFor="name" className="text-white">Qo'shimcha</label>
+                        <label htmlFor="name" className="text-white max-w-sm">Qo'shimcha</label>
                         <input type="text" className="focus:outline-none px-3 py-2" />
                     </div>
                     <button className="bg-white text-black px-2 py-2 mt-5 md:mt-0" data-aos="fade-left" data-aos-duration="900">Yuborish</button>
