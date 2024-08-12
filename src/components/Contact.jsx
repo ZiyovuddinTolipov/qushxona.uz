2389f9079aa51386fe6364dbb5e12b0c7f658ddb
ziyovuddintolipov <ziyovuddin.developer@gmail.com>
2024-08-12 16:29:42 +0500

    fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,8 +10,8 @@ const Contact = () => {
                     <h1 className="text-2xl md:text-3xl">#04 Biz bilan bog'lanish.</h1>
                     <div className=' mt-2 w-full'>
                         <div className="flex gap-x-2 w-full pr-1 galery">
-                            <img src={img} alt="" className="w-1/2 object-cover h-60"  data-aos="zoom-in" data-aos-duration="900"/>
-                            <img src={img} alt="" className="w-1/2 object-cover h-60"  data-aos="zoom-in" data-aos-duration="900"/>
+                            <img src={img} alt="" className="w-1/2 object-cover h-60" data-aos="zoom-in" data-aos-duration="900" />
+                            <img src={img} alt="" className="w-1/2 object-cover h-60" data-aos="zoom-in" data-aos-duration="900" />
                         </div>
                         <div className="mt-5">
                             <h2 className="text-base md:text-xl"></h2>
@@ -29,12 +29,12 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className='w-full md:w-1/2 h-full  flex flex-col justify-between '>
-                    <h2 className="text-base md:text-xl"  data-aos="fade-right" data-aos-duration="900">Bizning joylashuv</h2>
+                    <h2 className="text-base md:text-xl" data-aos="fade-right" data-aos-duration="900">Bizning joylashuv</h2>
                     <iframe
                         data-aos="zoom-in" data-aos-duration="1100"
                         src="https://www.google.com/maps?q=41.361945,69.373126&z=16&output=embed"
                         className=" w-full h-full mt-5"
-                        allowFullScreen loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                        allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade">
                     </iframe>
                 </div>
             </action>
