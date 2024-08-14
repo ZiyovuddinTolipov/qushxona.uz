2a0e43f4aecf6d798645792b4e0ae1d390cb9b3f
ziyovuddintolipov <ziyovuddin.developer@gmail.com>
2024-08-14 15:56:32 +0500

    fix bug deleivery number

--- src/components/Delivery.jsx
+++ src/components/Delivery.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-nonoctal-decimal-escape */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unescaped-entities */
 
 import { useState } from 'react';
 import toast from 'react-hot-toast';
-import ReactInputMask from 'react-input-mask';
+import InputMask from 'react-input-mask';
 
 const Delivery = () => {
     const [name, setName] = useState('');
@@ -15,40 +16,36 @@ const Delivery = () => {
         e.preventDefault();
 
         const botToken = '7274256076:AAGY9c8W8qzWY7lVrGEuNwjb2YFy-EzOmx8'; // Bu yerga bot tokeningizni yozing
-        const chatId = '2178285365'; // Bu yerga chat ID ni yozing
-        const message = `Ism: **${name}**\n
-                        Telefon: ${phone}\n
-                        Qo'shimcha: ${additional}`;
-        if (name && phone) {
-            try {
-                const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        chat_id: chatId,
-                        text: message
-                    }),
-                });
+        const chatId = '-1002178285365'; // Bu yerga chat ID ni yozing
+        const message = `Ism: ${name}\nTelefon: ${phone}\nQo'shimcha: ${additional}`;
 
-                if (response.ok) {
-                    toast.success('Ma\'lumotlar yuborildi!');
-                    // Formani tozalash
-                    setName('');
-                    setPhone('');
-                    setAdditional('');
-                } else {
-                    toast.error('Ma\'lumotlarni yuborishda xatolik yuz berdi.');
-                }
-            } catch (error) {
-                console.error('Xatolik:', error);
+        try {
+            console.log(message);
+            const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    chat_id: chatId,
+                    text: message
+                }),
+            });
+
+            if (response.ok) {
+                toast.success('Ma\'lumotlar yuborildi!');
+                // Formani tozalash
+                setName('');
+                setPhone('');
+                setAdditional('');
+            } else {
                 toast.error('Ma\'lumotlarni yuborishda xatolik yuz berdi.');
             }
+        } catch (error) {
+            console.error('Xatolik:', error);
+            toast.error('Ma\'lumotlarni yuborishda xatolik yuz berdi.');
         }
-        else {
-            toast.error("Xatolik:Ma'lumotlarni to'liq kiriting!")
-        }
+
     };
 
     return (
@@ -67,15 +64,15 @@ const Delivery = () => {
                     </div>
                     <div className="flex flex-col w-full max-w-sm text-black gap-y-1" data-aos="zoom-in" data-aos-duration="900">
                         <label htmlFor="phone" className="text-white max-w-sm">Telefon raqamingiz</label>
-                        <ReactInputMask
+                        <InputMask
                             value={phone}
                             onChange={(e) => setPhone(e.target.value)}
                             className="focus:outline-none px-3 py-2"
                             id="phone"
-                            mask="+\9\9\8 99 999 99 99"
-                            maskPlaceholder="Enter phone number"
+                            mask="+\9\9\8999999999"
                             required
                         />
+                        
                     </div>
                     <div className="flex flex-col w-full max-w-sm text-black gap-y-1" data-aos="zoom-in" data-aos-duration="900">
                         <label htmlFor="additional" className="text-white max-w-sm">Qo'shimcha</label>
