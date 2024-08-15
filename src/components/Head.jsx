940df0876caca166aec29fd0584ea51d41e9ffc9
ziyovuddintolipov <ziyovuddin.developer@gmail.com>
2024-08-15 12:55:36 +0500

    with seo

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,25 +1,46 @@
 import { CiCircleCheck } from "react-icons/ci";
+
+const services = [
+  "Aqiqa",
+  "Maraka",
+  "To’y hasham",
+  "Qurbonlik xizmati",
+];
+
 const Head = () => {
   return (
-    <section className="h-auto md:h-[90svh] text-white head-section relative pt-10 bg-gray-950" id="about">
+    <section
+      className="h-auto md:h-[90svh] text-white head-section relative pt-10 bg-gray-950"
+      id="about"
+    >
       <div className="content mx-auto max-w-[1200px] min-h-[70svh] md:h-[100svh] flex flex-row relative px-5">
-        <div className="w-1/2  md:w-1/2 flex flex-col justify-around py-5 md:py-0 gap-y-3 md:gap-y-0">
-          <div className='text-white mb-4' data-aos="fade-right" data-aos-duration="900">
-            <h2 className="text-2xl" data-aos="fade-right">Qurbonlik xizmati.</h2>
-            <ul className='mt-5 text-xl'>
-              <li ><CiCircleCheck className='inline' /> Aqiqa</li>
-              <li ><CiCircleCheck className='inline' /> Maraka</li>
-              <li ><CiCircleCheck className='inline' /> To’y hasham</li>
-              <li ><CiCircleCheck className='inline' /> Qurbonlik xizmati</li>
+        <div className="w-1/2 md:w-1/2 flex flex-col justify-around py-5 md:py-0 gap-y-3 md:gap-y-0">
+          <div
+            className="text-white mb-4"
+            data-aos="fade-right"
+            data-aos-duration="900"
+          >
+            <h2 className="text-2xl" data-aos="fade-right">
+              Qurbonlik xizmati.
+            </h2>
+            <ul className="mt-5 text-xl">
+              {services.map((service, index) => (
+                <li key={index}>
+                  <CiCircleCheck className="inline" /> {service}
+                </li>
+              ))}
             </ul>
           </div>
         </div>
-        <div className='flex items-center justify-center  w-1/2'>
-          <video className="max-h-[500px]" src="https://www.dropbox.com/scl/fi/dlfgbncqr8pwf1hmod69a/qushxona.uz-20240814-0004.mp4?rlkey=jh3j8eyei9nxffsmt2boeb9uw&dl=1" autoPlay controls></video>
+        <div className="flex items-center justify-center w-1/2">
+          <video  className="max-h-[400px] h-full w-[300px]" controls>
+            <source src="https://www.dropbox.com/scl/fi/t4t43kqtwc3knjiqqt34u/qushxona.uz-20240814-0003.mp4?rlkey=6lmzu16izpm9qwolrwxzxvfxp&st=u818syax&dl=1" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Head
+export default Head;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,38 +7,42 @@ export default function NavBar() {
 
     useEffect(() => {
         const handleScroll = () => {
-            if (window.scrollY > 0) {
-                setHasShadow(true);
-            } else {
-                setHasShadow(false);
-            }
+            setHasShadow(window.scrollY > 0);
         };
 
         window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
+        return () => window.removeEventListener('scroll', handleScroll);
     }, []);
-    return (
-        <nav className={`sticky top-0 shadow-md  h-[10svh] z-50 ${hasShadow ? 'nav-shadow' : ''} navbar`} id='navbar navbar'>
-            <div className="md:justify-between flex  mx-auto  md:items-center flex-col md:flex-row max-w-[1200px]  rounded-xl">
-                <div className="w-[100%] md:w-auto">
-                    <div className="flex items-center justify-center  md:block">
-                        <a href="" className='flex items-center text-2xl text-[#8d0b0d] font-semibold uppercase' >
-                            <img src={logo} alt="qushxona.uz logo" className='h-16 nav-logo' loading='lazy' />
-                            <span  >Qushxona.uz</span>
-                        </a>
 
-                    </div>
+    return (
+        <nav 
+            className={`sticky top-0 z-50 h-[10svh] ${hasShadow ? 'shadow-md' : ''} navbar`} 
+            id="navbar"
+        >
+            <div className="max-w-[1200px] mx-auto flex flex-col md:flex-row md:justify-between md:items-center rounded-xl">
+                <div className="w-full md:w-auto flex justify-center md:block">
+                    <a href="/" className="flex items-center text-2xl text-[#8d0b0d] font-semibold uppercase">
+                        <img 
+                            src={logo} 
+                            alt="Qushxona.uz - Qassoblik va Qurbonlik Xizmati Logosi" 
+                            className="h-16 nav-logo" 
+                            loading="lazy" 
+                        />
+                        <span>Qushxona.uz</span>
+                    </a>
                 </div>
 
-                <ul className="flex px-5 justify-around text-base md:text-xl font-medium uppercase pr-5 bg-[#F9F0E7] border-t border-t-[#8d0b0d] md:border-t-0 text-[#8d0b0d]">
-                    <li className="border-r-2 md:border-0 border-[#8d0b0d] px-5 py-3" > <a href='#service' className='nav-links'>xizmatlarimiz</a></li>
-                    <li className="border-r-2 md:border-0 border-[#8d0b0d] px-5 py-3"><a href='#delivery' className='nav-links'>Buyurtma</a></li>
-                    <li className='px-5 py-3'><a href='#contact' className='nav-links'>Bog'lanish</a></li>
+                <ul className="flex justify-around text-base md:text-xl font-medium uppercase bg-[#F9F0E7] border-t border-[#8d0b0d] md:border-t-0 text-[#8d0b0d]">
+                    <li className="px-5 py-3 border-r-2 md:border-0 border-[#8d0b0d]">
+                        <a href="#service" className="nav-links">Xizmatlarimiz</a>
+                    </li>
+                    <li className="px-5 py-3 border-r-2 md:border-0 border-[#8d0b0d]">
+                        <a href="#delivery" className="nav-links">Buyurtma</a>
+                    </li>
+                    <li className="px-5 py-3">
+                        <a href="#contact" className="nav-links">Bog'lanish</a>
+                    </li>
                 </ul>
-
             </div>
         </nav>
     );
